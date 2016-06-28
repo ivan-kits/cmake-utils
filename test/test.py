c22538069e1d0acd3cf52a6bd34c734648ab60e1
Chaoren Lin <aoe@google.com>
2016-06-27 19:19:36 -0700

    Add testing for the generated JSON file.
    Change-Id: Ia1dbc27f6b579d0e6bbc8ade913a28bb4f4f1a41

--- test/test.py
+++ test/test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+import json
 import os
+import shlex
 import shutil
 import subprocess
 import unittest
@@ -22,7 +24,7 @@ toolchain_file = os.path.join(cmake_root, 'android.toolchain.cmake')
 ndk_build = os.path.join(ndk, 'ndk-build')
 
 
-class TestToolchainFile(unittest.TestCase):
+class TestCMake(unittest.TestCase):
     def make_build_directories(self):
         build = os.path.join(tmp, self.toolchain, self.abi,
                              self.platform, self.stl, self.build_type)
@@ -46,6 +48,86 @@ class TestToolchainFile(unittest.TestCase):
                          msg='\n%s\n%s\n%s' %
                          (str.join(' ', command), stdout, stderr))
 
+    def check_json_library(self, key, value, toolchain_key):
+        split_key = key.split('-', 2)
+        self.assertEqual(len(split_key), 3)
+        [name, build_type, abi] = split_key
+        self.assertEqual(build_type, self.build_type)
+        self.assertEqual(abi, self.abi)
+        self.assertEqual(value['abi'], abi)
+        self.assertEqual(value['artifactName'], name)
+        self.assertEqual(value['buildCommand'],
+                         '%s --build %s --target %s' %
+                         (cmake, self.cmake_build, name))
+        self.assertEqual(value['buildType'], build_type)
+        files = value['files']
+        self.assertEqual(len(files), 1)
+        flags = files[0]['flags']
+        self.assertTrue(flags)
+        tokenized_flags = shlex.split(flags)
+        self.assertTrue('-DANDROID' in tokenized_flags)
+        self.assertEqual('-DNONE' in tokenized_flags,
+                         self.stl == 'none')
+        self.assertEqual('-DSYSTEM' in tokenized_flags,
+                         self.stl == 'system')
+        self.assertEqual('-DNEON' in tokenized_flags,
+                         self.abi == 'armeabi-v7a' and
+                         name.startswith('neon_'))
+        split_name = name.split('_', 2)
+        if 'neon' in split_name:
+            self.assertEqual(len(split_name), 3)
+            split_name.remove('neon')
+        self.assertEqual(len(split_name), 2)
+        split_name.reverse()
+        source = '.'.join(split_name)
+        if 'exe' not in split_name:
+            source = os.path.join(split_name[0], source)
+        source = os.path.join(project, source)
+        self.assertEqual(files[0]['src'], source)
+        self.assertEqual(files[0]['workingDirectory'], self.cmake_build)
+        if name.endswith('_exe'):
+            output = name
+        elif name.endswith('_shared'):
+            output = 'lib%s.so' % name
+        elif name.endswith('_static'):
+            output = 'lib%s.a' % name
+        self.assertEqual(value['output'],
+                         os.path.join(self.cmake_build, output))
+        self.assertEqual(value['toolchain'], toolchain_key)
+
+    def check_json(self, json_file):
+        json_obj = json.load(open(json_file))
+        libraries = ['c_exe', 'c_shared', 'c_static', 'cpp_exe']
+        c_file_extensions = ['c']
+        cpp_file_extensions = ['cpp']
+        if self.stl not in ['none', 'system']:
+            libraries += ['cpp_shared', 'cpp_static']
+        if self.abi == 'armeabi-v7a':
+            libraries += ['neon_c_exe', 'neon_cpp_exe']
+        self.assertEqual(json_obj['buildFiles'],
+                         [os.path.join(project, 'CMakeLists.txt')])
+        self.assertEqual(json_obj['cFileExtensions'],
+                         c_file_extensions)
+        self.assertEqual(json_obj['cppFileExtensions'],
+                         cpp_file_extensions)
+        self.assertEqual(json_obj['cleanCommands'],
+                         ['%s --build %s --target clean' %
+                          (cmake, self.cmake_build)])
+        self.assertEqual(len(json_obj['toolchains']), 1)
+        toolchain_key = json_obj['toolchains'].keys()[0]
+        toolchain = json_obj['toolchains'].values()[0]
+        self.assertTrue(toolchain['cCompilerExecutable'].startswith(
+            os.path.join(ndk, 'toolchains')))
+        self.assertTrue(toolchain['cppCompilerExecutable'].startswith(
+            os.path.join(ndk, 'toolchains')))
+        self.assertTrue(toolchain['cppCompilerExecutable'].endswith('++'))
+        self.assertEqual(len(json_obj['libraries']), len(libraries))
+        for library in libraries:
+            key = '%s-%s-%s' % (library, self.build_type, self.abi)
+            self.assertTrue(key in json_obj['libraries'])
+        for key, value in json_obj['libraries'].iteritems():
+            self.check_json_library(key, value, toolchain_key)
+
     def compile_cmake(self):
         command = [cmake,
                    '-H%s' % project,
@@ -60,6 +142,8 @@ class TestToolchainFile(unittest.TestCase):
                    '-DANDROID_STL=%s' % self.stl,
                    '-DCMAKE_BUILD_TYPE=%s' % self.build_type]
         self.run_and_assert_success(command)
+        json_file = os.path.join(self.cmake_build, 'android_gradle_build.json')
+        self.check_json(json_file)
         command = [cmake, '--build', self.cmake_build]
         self.run_and_assert_success(command)
 
@@ -123,7 +207,7 @@ def generate_all_tests():
                                                         build_type)
                         test = generate_test(toolchain, abi, platform,
                                              stl, build_type)
-                        setattr(TestToolchainFile, name, test)
+                        setattr(TestCMake, name, test)
 
 
 if __name__ == '__main__':
