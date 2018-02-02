063cde5842c2575786a9b9580cf03791d5190f65
Jomo Fisher <jomof@google.com>
2018-02-02 14:04:47 -0800

    More diagnostic
    Change-Id: I6932d129c5399facf071f3183b21dd63bb56bc46

--- build.sh
+++ build.sh
@@ -110,7 +110,9 @@ case "$OS" in
 		reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
         if %OS%==32BIT echo This is a 32bit operating system
         if %OS%==64BIT echo This is a 64bit operating system
-		%CMAKE% --version
+		"%CMAKE%" --version
+		"%CMAKE%" --version > look.txt
+		type look.txt
 		%CMAKE% $(printf '"%s" ' "${CMAKE_OPTIONS[@]}")
 		set DESTDIR=%INSTALL%
 		%CMAKE% --build %BUILD% --target install
